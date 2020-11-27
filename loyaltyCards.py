83da1edab3d07d34ef95437a58224d1b675f3ff4
joan.iscmail@gmail.com <joanisc@Calma01.espiga>
2020-11-27 20:50:07 +0100

    Added edit and Delete again

--- loyaltyCards.py
+++ loyaltyCards.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python3
-
-import gi, sqlite3 as sqlite
+# -*- coding: utf-8 -*-
+import gi, sqlite3 as sqlite, time
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, GdkPixbuf
+from gi.repository import Gtk, GdkPixbuf, GLib
 
-con = sqlite.connect('/usr/share/loyaltycardsopen/loyaltyCardsDb.db')
+con = sqlite.connect('loyaltyCardsDb.db')
 
 class ListBoxRowWithData(Gtk.ListBoxRow):
     def __init__(self, data):
@@ -13,6 +12,9 @@ class ListBoxRowWithData(Gtk.ListBoxRow):
         self.add(Gtk.Label(label=data))
 
 class Handler:
+    global comeFromEdit 
+    comeFromEdit = 0
+
     def onDestroy(self, *args):
         Gtk.main_quit()
 
@@ -21,16 +23,16 @@ class Handler:
         img = builder.get_object("img")
         frontImage = builder.get_object("frontImage")
         pathFront = frontImage.get_filename()
-        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(filename=pathFront, width=60, height=60, preserve_aspect_ratio=True)
+        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(filename=pathFront, width=80, height=60, preserve_aspect_ratio=True)
         img.set_from_pixbuf(pixbuf)
         img.show_all()
 
     def on_file_selected_back(self, *args):
-        print("File selected!")
+        print("Back File selected!")
         imgBack = builder.get_object("imgBack")
         backImage = builder.get_object("backImage")
         pathBack = backImage.get_filename()
-        pixbuf1 = GdkPixbuf.Pixbuf.new_from_file_at_scale(filename=pathBack, width=60, height=60, preserve_aspect_ratio=True)
+        pixbuf1 = GdkPixbuf.Pixbuf.new_from_file_at_scale(filename=pathBack, width=80, height=60, preserve_aspect_ratio=True)
         imgBack.set_from_pixbuf(pixbuf1)
         imgBack.show_all()
 
@@ -54,60 +56,152 @@ class Handler:
                 listbox.add(ListBoxRowWithData(rowData))
         listbox.show_all()
 
-    def row_selected(cur, image, row):
+    def row_selected(cur, self, row):
+        global stringId
+        global cardName
+        global codebar
+        global photoPath
         image = builder.get_object("image")
-        if row.data is not None:
-            rowData = row.data
-            rowData = rowData.strip()
-            rowData = " ".join(rowData.split())
-            cardName, codebar = rowData.split()
-            print("cardName:"+cardName)
-            print("codebar:"+codebar)
+        rowData = row.data
+        rowData = rowData.strip()
+        rowData = " ".join(rowData.split())
+        cardName, codebar = rowData.split()
+        print("cardName:"+cardName)
+        print("codebar:"+codebar)
                
         with con:
             cur = con.cursor()
-            cur.execute("SELECT ID, IMAGE FROM CARD where CARD_NAME = ? LIMIT 1" , (cardName, ))
+            cur.execute("SELECT ID, IMAGE FROM CARD where CARD_NAME = ? LIMIT 1" , (cardName,))
             row = cur.fetchone()
-            id = row[0]
+            id = row[0] 
+            stringId = ""+str(id)+""
+            print("id0:"+stringId)
             photo = row[1]
             photoPath = ""+str(id)+".jpg"
             with open(photoPath, 'wb') as file:
                 file.write(photo)
                 print("Stored blob data into: ", photoPath, "\n")
-        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(filename=photoPath, width=100, height=100, preserve_aspect_ratio=True)
+            pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(filename=photoPath, width=80, height=60, preserve_aspect_ratio=True)
 
         image.set_from_pixbuf(pixbuf)
         image.show_all()
-        
-    def on_button_clicked(self, button):
+        delete.show()
+        edit.show()
+
+    def edit_clicked_cb(self, notebook):
+        notebook = builder.get_object("notebook")
+        notebook.next_page()
+        global comeFromEdit
+        comeFromEdit = 1
+        print ("comeFromEdit:"+str(comeFromEdit))
+        id = stringId
+        print("id:"+id)
         entry = builder.get_object("cardNameEntry")
         img = builder.get_object("img")
         barcodeEntry = builder.get_object("barcodeEntry")
         frontImage = builder.get_object("frontImage")
         backImage = builder.get_object("backImage")
+        imgBack = builder.get_object("imgBack")
         pathFront = frontImage.get_filename()
         pathBack = backImage.get_filename()
-        with open(pathFront, 'rb') as input_file:
-            pathFrontBlob = input_file.read()
-        with open(pathBack, 'rb') as input_file1:
-            pathBackBlob = input_file1.read()
-        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(filename=pathFront, width=100, height=100, preserve_aspect_ratio=True)
+        entry.set_text(cardName)
+        barcodeEntry.set_text(codebar)
+        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(filename=photoPath, width=80, height=60, preserve_aspect_ratio=True)
         img.set_from_pixbuf(pixbuf)
-                
-        cardName =str(entry.get_text())
-        barcodeEntry =str(barcodeEntry.get_text())
-        print ('Card Name: %s' % cardName + ' '+ 'barcodeEntry: %s' % barcodeEntry)
+        pixbuf1 = GdkPixbuf.Pixbuf.new_from_file_at_scale(filename=photoPath, width=80, height=60, preserve_aspect_ratio=True)
+        imgBack.set_from_pixbuf(pixbuf1)
+        img.show_all()
+        imgBack.show_all()
         
+    def on_button_clicked(self, button):
+        global comeFromEdit
+        entry = builder.get_object("cardNameEntry")
+        img = builder.get_object("img")
+        imgBack = builder.get_object("imgBack")
+        barcodeEntry = builder.get_object("barcodeEntry")
+        frontImage = builder.get_object("frontImage")
+        backImage = builder.get_object("backImage")
+        savedImageInfo = builder.get_object("savedImageInfo")
+        
+        pathFront = frontImage.get_filename()
+        pathBack = backImage.get_filename()
+        if pathFront is not None:
+            with open(pathFront, 'rb') as input_file:
+                pathFrontBlob = input_file.read()
+        else:
+            pathFrontBlob = ''
+        if pathBack is not None:
+            with open(pathBack, 'rb') as input_file1:
+                pathBackBlob = input_file1.read()
+        else:
+            pathBackBlob = ''
+        cardName =str(entry.get_text())
+        barcodeEntryStr =str(barcodeEntry.get_text())
+        print ('Card Name: %s' % cardName + ' '+ 'barcodeEntry: %s' % barcodeEntryStr)
+        print ("comeFromEditSave:"+str(comeFromEdit))
         with con:
             cur = con.cursor()
-            cur.execute('INSERT INTO CARD(CARD_NAME, BARCODE, IMAGE, IMAGEBACK) VALUES (?,?,?,?)', (cardName, barcodeEntry, sqlite.Binary(pathFrontBlob), sqlite.Binary(pathBackBlob)))
+            if comeFromEdit == 0:
+                print ("If =>INSERT")
+                cur.execute('INSERT INTO CARD(CARD_NAME, BARCODE, IMAGE, IMAGEBACK) VALUES (?,?,?,?)', (cardName, barcodeEntryStr, sqlite.Binary(pathFrontBlob), sqlite.Binary(pathBackBlob)))
+                print(cur)
+                
+            else:
+                print("Else => UPDATE")
+                sql =cur.execute("UPDATE CARD SET CARD_NAME = ?, BARCODE = ?, IMAGE = IFNULL(?,''), IMAGEBACK = IFNULL(?,'') WHERE ID = ?" , (cardName, barcodeEntry, sqlite.Binary(pathFrontBlob), sqlite.Binary(pathBackBlob), stringId))
+                print(sql)
+                comeFromEdit = 0
+                
+        cardName = entry.set_text('')
+        barcodeEntry = barcodeEntry.set_text('')
+        pathFront = frontImage.unselect_all()
+        pathBack = backImage.unselect_all()
+        img.hide()
+        imgBack.hide()
+        GLib.timeout_add_seconds(1, 3, self.show_saved_image_seconds())
+        savedImageInfo.hide()
+        
+     
+    def entryCardsTab_activate_current_link_cb(self, cur, button):
+        print ('Accessed entryCardsTab:')
 
+    def show_saved_image_seconds(self):
+        print ('Accessed show_saved_image_seconds:')
+        savedImageInfo.show()
+    
+    def del_clicked_cb(self, cur):
+        popDelConfirm = builder.get_object("popDelConfirm")
+        popDelConfirm.show_all()
+        popDelConfirm.popup()
+
+    def del_yes_clicked_cb(self, cur):
+        popDelConfirm = builder.get_object("popDelConfirm")
+        id = stringId
+        print("id:"+id)
+        with con:
+            cur = con.cursor()
+            cur.execute('DELETE FROM CARD WHERE ID = ? ', (id,))
+            print(cur)
+        popDelConfirm.hide()
+    
+    def del_no_clicked_cb(self, cur):
+        popDelConfirm = builder.get_object("popDelConfirm")
+        popDelConfirm.hide()
+         
 
 builder = Gtk.Builder()
-builder.add_from_file("/usr/lib/loyaltycardsopen/gladeWindowDesign.glade")
+builder.add_from_file("gladeWindowDesign.glade")
 builder.connect_signals(Handler())
 
 window = builder.get_object("window1")
+delete = builder.get_object("del")
+edit = builder.get_object("edit")
+savedImageInfo = builder.get_object("savedImageInfo")
+
 window.show_all()
+delete.hide()
+edit.hide()
+savedImageInfo.hide()
+
 
 Gtk.main()
